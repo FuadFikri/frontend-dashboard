ac76a92d06e610f81626c731bf160c299dcbc40d
Fikri <fuadfikri01@gmail.com>
2019-04-10 16:06:55 +0700

    add notify when insert, update, delete

--- src/app/views/administration/role-dashboard/role-dashboard.component.ts
+++ src/app/views/administration/role-dashboard/role-dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ElementRef, Inject } from '@angular/core';
 import { RoleDashboardService } from './role-dashboard.service';
 import { Dashboard } from "./dashboard.model";
-
+import notify from 'devextreme/ui/notify';
 @Component({
   selector: 'app-role-dashboard',
   templateUrl: './role-dashboard.component.html',
@@ -13,6 +13,15 @@ export class RoleDashboardComponent implements OnInit {
   gridDataSource:any;
   Role:any;
   data:Dashboard;
+
+  options = {
+    message: '',
+    closeOnOutsideClick: true,
+    closeOnClick: true,
+    closeOnSwipe: true,
+    closeOnBackButton: true,
+  };
+
   constructor(private elementRef: ElementRef,
     @Inject(RoleDashboardService) private roleDashboardService: RoleDashboardService) { 
         this.roleDashboardService.getAll().subscribe(resp => {
@@ -33,10 +42,17 @@ export class RoleDashboardComponent implements OnInit {
     this.data = e.data;
     this.roleDashboardService.save(this.data).subscribe(res=> {
       if(res.d==1){
+        this.options.message = 'New Dashboard Role Saved ';
+        notify(this.options, 'success', 3000);
         console.log("insert success");
       }else{
+        this.options.message = 'Saving Failed';
+          notify(this.options, 'error', 3000);
         console.log("insert failed ");
       }
+    }, err => {
+      this.options.message = 'Saving Failed';
+      notify(this.options, 'error', 3000);
     });
   } 
   
@@ -45,10 +61,17 @@ export class RoleDashboardComponent implements OnInit {
     this.data.dashboard_id = e.oldData.dashboard_id;
     this.roleDashboardService.update(this.data).subscribe(res=> {
       if(res.d==1){
-        console.log("update success", this.data);
+        this.options.message = 'Dashboard Role Updated';
+        notify(this.options, 'success', 3000);
+        console.log("updating success",this.data);
       }else{
-        console.log("update failed ");
+        this.options.message = 'updating Failed';
+          notify(this.options, 'error', 3000);
+        console.log("updating failed ");
       }
+    }, err => {
+      this.options.message = 'updating Failed';
+      notify(this.options, 'error', 3000);
     });
   }
 
@@ -56,10 +79,17 @@ export class RoleDashboardComponent implements OnInit {
     let dashboard_id = e.key;
     this.roleDashboardService.delete(dashboard_id).subscribe(res=> {
       if(res.d==1){
-        console.log("deleting success on id", dashboard_id);
+        this.options.message = 'Dashboard Role Deleted';
+        notify(this.options, 'success', 3000);
+        console.log("deleting success ",dashboard_id);
       }else{
+        this.options.message = 'deleting Failed';
+          notify(this.options, 'error', 3000);
         console.log("deleting failed ");
       }
+    }, err => {
+      this.options.message = 'deleting Failed';
+      notify(this.options, 'error', 3000);
     });
   }
 
