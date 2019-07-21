8559523852b226fb013720b62220293f4f6bdbe4
Fikri <fuadfikri01@gmail.com>
2019-07-21 20:18:46 +0700

    all data sbu

--- src/app/views/administration/balanced-scorecard-sbu/data-sbu/data-sbu.component.ts
+++ src/app/views/administration/balanced-scorecard-sbu/data-sbu/data-sbu.component.ts
@@ -1,9 +1,10 @@
 import {
   Component,
   OnInit,
-  OnDestroy
+  OnDestroy,
+  ViewChild
 } from '@angular/core';
-
+import notify from 'devextreme/ui/notify';
 import DataSource from 'devextreme/data/data_source';
 import ArrayStore from 'devextreme/data/array_store';
 import {
@@ -16,6 +17,9 @@ import 'rxjs/add/operator/filter';
 import {
   BalancedScorecardService
 } from '../../balanced-scorecard-setting/balanced-scorecard.service';
+import { DxDataGridComponent, DxFileUploaderComponent } from 'devextreme-angular';
+import { Nilai } from '../../balanced-scorecard-setting/Model';
+import { AppConstant } from 'app/app.constant';
 
 @Component({
   selector: 'app-data-sbu',
@@ -24,6 +28,9 @@ import {
   providers: [BalancedScorecardService]
 })
 export class DataSbuComponent implements OnInit {
+  @ViewChild(DxDataGridComponent) dataGrid: DxDataGridComponent;
+  @ViewChild(DxFileUploaderComponent) fileUploader: DxFileUploaderComponent;
+  nilai: Nilai;
   cardBarSource;
   cardBars;
   subscription1: Subscription;
@@ -31,19 +38,46 @@ export class DataSbuComponent implements OnInit {
   perspektifSource;
   bulanDropDown;
   tahun;
-  daerah;
-  constructor(private service: BalancedScorecardService, private route: ActivatedRoute) {
+  popupVisible:boolean=false;
+  buttonMode="outlined"
+  formData: FormData;
+  file:any;
+  bulan: string;
+
+  options = {
+    message: '',
+    closeOnOutsideClick: true,
+    closeOnClick: true,
+    closeOnSwipe: true,
+    closeOnBackButton: true,
+  };
+  totalBobot: any;
+  id_nilai: any;
+  disableBtUpload: boolean=true;
+  resourceUrlRole: string;
+  now: Date;
+
+  bulanParams:any;
+  tahunParams:any;
+  
+  daerah: string;
+
+  constructor(private service: BalancedScorecardService, private route: ActivatedRoute,private a: AppConstant) {
     this.cardBarSource = [];
+    this.resourceUrlRole= a.SERVER_URL;
+    this.now = new Date();
   }
 
 
   ngOnInit() {
     let daerah = this.route.snapshot.paramMap.get("daerah");
+    
     this.daerah = daerah;
     this.route.queryParams.filter(params => params.tahun)
       .subscribe(params => {
-
-        this.subscription1 = this.service.getCardBarByTahunDanBulanLevelSatu(params.tahun, params.bulan, daerah).subscribe(resp => {
+        this.bulanParams = params.bulan;
+        this.tahunParams = params.tahun;
+        this.subscription1 = this.service.getCardBarByTahunDanBulanLevelSatu(params.tahun, params.bulan,daerah).subscribe(resp => {
           // object to array
           this.cardBars = Object.keys(resp.d).map(function (index) {
             let card = resp.d[index];
@@ -80,9 +114,231 @@ export class DataSbuComponent implements OnInit {
     return item.dataSourceInstance;
   }
 
-  // ngOnDestroy(): void {
-  //   this.subscription1.unsubscribe();
-  //   this.subscription2.unsubscribe();
-  // }
+  ngOnDestroy(): void {
+    this.subscription1.unsubscribe();
+    this.subscription2.unsubscribe();
+  }
+
+
+
+
+  hitungPersentase(e) {
+    let hasil;
+    if (this.nilai.target_bulanan && this.nilai.realisasi) {
+      hasil = (this.nilai.realisasi / this.nilai.target_bulanan) * 100;
+    } else if (this.nilai.target_bulanan) {
+      if (!e.oldData.realisasi) {
+        hasil = 0;
+      }else {
+        hasil = (e.oldData.realisasi / this.nilai.target_bulanan) * 100
+      }
+    } else if (this.nilai.realisasi) {
+      if (!e.oldData.target_bulanan) {
+        hasil = 0;
+      }else {
+        hasil = (this.nilai.realisasi / e.oldData.target_bulanan) * 100;
+      }
+    }
+    console.log('hasil', hasil);
+    this.nilai.persentase = hasil.toFixed();
+    return hasil;
+  }
+  hitungPersentasePolarisasiNegatif(e) {
+    let hasil;
+    if (this.nilai.target_bulanan && this.nilai.realisasi) {
+      hasil = (2-(this.nilai.realisasi / this.nilai.target_bulanan)) * 100;
+    } else if (this.nilai.target_bulanan) {
+      if (!e.oldData.realisasi) {
+        hasil = 0;
+      }else {
+        hasil = (2-(e.oldData.realisasi / this.nilai.target_bulanan)) * 100;
+      }
+    } else if (this.nilai.realisasi ) {
+      if (!e.oldData.target_bulanan) {
+        hasil = 0;
+      }else {
+        hasil = (2-(this.nilai.realisasi / e.oldData.target_bulanan)) * 100;
+      }
+    }
+    console.log('persentase', hasil);
+    if(hasil < 0){
+      hasil = '0';
+      this.nilai.persentase = hasil;
+    } else {
+      this.nilai.persentase = hasil.toFixed();
+    }
+    return hasil;
+  }
+
+  hitungNilai(e, persentase) {
+    let nilai: any;
+    if (persentase >= 100) {
+      nilai = new Number(e.oldData.bobot);
+      console.log('nilaiBaik', nilai);
+      this.nilai.keterangan = 'Baik'
+    } else {
+      nilai = e.oldData.bobot * this.nilai.persentase / 100;
+      this.nilai.keterangan = 'Masalah'
+      console.log('nilaiMasalah', nilai);
+    }
+
+    return nilai;
+  }
+
+  updateCardBar(e) {
+    this.nilai = e.newData;
+    this.nilai.id_nilai = e.oldData.id_nilai;
+    console.log(this.nilai);
+    if (this.nilai.realisasi===0 || this.nilai.target_bulanan ===0) {
+      this.nilai.realisasi = this.nilai.realisasi.toString();
+      this.nilai.target_bulanan = this.nilai.target_bulanan.toString();
+    }
+    
+    if (this.nilai.realisasi || this.nilai.target_bulanan) {
+
+      let persentase;
+      if (e.oldData.polarisasi_id=="1") {
+        persentase = this.hitungPersentase(e);
+      } else {
+        persentase = this.hitungPersentasePolarisasiNegatif(e);
+      }
+
+      const nilai = this.hitungNilai(e, persentase);
+
+      this.nilai.persentase = this.nilai.persentase.toString();
+      if (this.nilai.realisasi) {
+        this.nilai.realisasi = this.nilai.realisasi.toString();
+      }
+      if (!this.nilai.target_bulanan) {
+        if (e.oldData.target_bulanan) {
+          this.nilai.target_bulanan = e.oldData.target_bulanan.toString();
+        }
+      } else {
+        this.nilai.target_bulanan = this.nilai.target_bulanan.toString();
+      }
+      this.nilai.nilai = nilai.toPrecision(3);
+      this.nilai.nilai = this.nilai.nilai.toString();
+    }
+
+    console.log('nilai', this.nilai);
+    this.service.updateNilai(this.nilai).subscribe(res => {
+      if (res.d == 1) {
+        this.dataGrid.instance.collapseAll(-1);
+        this.refresh();
+        this.options.message = 'Success Updated';
+        notify(this.options, 'success', 3000);
+        console.log('updating success', this.nilai);
+      } else {
+        this.options.message = 'updating Failed';
+        notify(this.options, 'error', 3000);
+        console.log('updating failed ', res);
+      }
+    }, err => {
+      this.options.message = 'updating Failed';
+      notify(this.options, 'error', 3000);
+      console.log('updating failed ', err);
+
+    });
+  }
+
+
+  customizeBobot(data) {
+    this.totalBobot += data.value;
+    return 'Bobot : ' + data.value;
+  }
+  customizeNilai(data) {
+    return 'Nilai : ' + data.value.toPrecision(3);
+  }
+
+  persen(cellInfo) {
+    if (cellInfo.value) {
+      return cellInfo.value + '%'
+    }
+  }
+
+  judulPopup="Upload File";
+  openModal(cell) {
+    this.popupVisible = true;
+    this.id_nilai = cell.value;
+  }
+
+    // ketika modal diclose maka uploader di reset
+    close(e) {
+      this.fileUploader.instance.reset();
+    }
+    
+    
+    
+  
+    uploadFile(e) {
+      
+      e.preventDefault;
+      console.log(e);
+      this.popupVisible = false;
+  
+      
+      this.formData = new FormData();
+      this.formData.append("id_nilai",this.id_nilai);
+      this.formData.append("file",this.file);
+  
+  
+      this.service.upload(this.formData).subscribe(res => {
+        if (res.d == 1 && res.s == 200) {
+          this.popupVisible = false;
+          this.formData = new FormData();
+          this.fileUploader.instance.reset();
+          
+          this.dataGrid.instance.collapseAll(-1);
+          this.refresh();
+          this.options.message = 'Upload Berhasil';
+          notify(this.options, 'success', 3000);
+          console.log('updating success', res);
+        } else {
+          this.options.message = 'Upload Gagal';
+          notify(this.options, 'error', 3000);
+          console.log('updating failed ', res);
+        }
+      }, err => {
+        this.options.message = 'Creating Failed';
+        notify(this.options, 'server error', 3000);
+        console.log('server error ', err);
+      });
+    }
+  
+    selectFile(e) {
+      console.log(e.target.files)
+      this.file = e.target.files[0]
+      if((this.file.size/1048576)>20){
+        console.log("file size", this.file.size/1024);
+        this.disableBtUpload = true;
+      }else{
+        console.log("file size", this.file.size/1048576);
+        this.disableBtUpload = false;
+      }
+    }
+    openInNewTab(url:any) {
+      // open link in new tab
+      let fullUrl= this.resourceUrlRole+ "/"+url;
+      const newTab = window.open(fullUrl, '_blank')
+      // set opener to null so that no one can references it
+      newTab.opener = null
+  }
+  
+  refresh() {
+    this.bulan = this.bulanDropDown[(this.now.getMonth())-1].bulan;
+      const tahun = this.now.getFullYear().toString();
+      this.cardBarSource = [];
+    
+    this.service.getCardBarByTahunDanBulanLevelSatu(this.tahunParams, this.bulanParams,this.daerah).subscribe(resp => {
+      // object to array
+      this.cardBars = Object.keys(resp.d).map(function (index) {
+        const card = resp.d[index];
+        return card;
+      });
+      console.log('cards', this.cardBars);
+    });
+    this.dataGrid.instance.refresh();
+  }
+
 
 }
