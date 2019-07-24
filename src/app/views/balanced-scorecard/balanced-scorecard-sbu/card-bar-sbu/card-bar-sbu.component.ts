5381327331149d5cea4d5dc024089e186b534bd7
Fikri <fuadfikri01@gmail.com>
2019-07-24 11:33:02 +0700

    kpi subdir cardbar sbu

--- src/app/views/balanced-scorecard/balanced-scorecard-sbu/card-bar-sbu/card-bar-sbu.component.ts
+++ src/app/views/balanced-scorecard/balanced-scorecard-sbu/card-bar-sbu/card-bar-sbu.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-card-bar-sbu',
+  selector: 'card-bar-sbu',
   templateUrl: './card-bar-sbu.component.html',
   styleUrls: ['./card-bar-sbu.component.scss']
 })
